39d2216c8c9cbff002fe6e34ecf766265d910cc0
Derrick Johnson <10237736+dj51234@users.noreply.github.com>
2021-02-19 18:50:42 -0500

    added proptypes

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,10 +1,17 @@
+import Card from "./shared/Card";
+import { PropTypes } from "prop-types";
+
 function FeedbackItem({ item }) {
   return (
-    <div className="card">
+    <Card>
       <div className="card__num">{item.rating}</div>
       <div className="card__text">{item.text}</div>
-    </div>
+    </Card>
   );
 }
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+};
 
 export default FeedbackItem;
