e06e19eb8dc013952fe912064564398fd60d82ae
Derrick Johnson <10237736+dj51234@users.noreply.github.com>
2021-03-17 20:27:55 -0400

    Rating component

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,37 +1,31 @@
 import { useState } from "react";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
+import RatingSelector from "./RatingSelector";
 
 function FeedbackForm() {
   const [content, setContent] = useState("");
+  const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
-  const [warning, setWarning] = useState(
-    "Review must be at least 10 characters long"
-  );
+  const [warning, setWarning] = useState(`Review must be over 10 characters`);
 
   const handleContentChange = (e) => {
     if (content === "") {
       setBtnDisabled(true);
-      setContent(null);
-    } else if (content !== "" && content.trim().length < 9) {
+      setWarning(warning);
+    } else if (content.length > 0 && content.length < 9) {
       setWarning(warning);
     } else {
       setBtnDisabled(false);
-      setWarning("");
+      setWarning(null);
     }
     setContent(e.target.value);
-    // if (content === "") {
-    //   setBtnDisabled(true);
-    // } else if (content.trim().length > 0 && content.trim().length < 10) {
-    //   setWarning(`Must add ${10 - content.trim().length} more characters`);
-    // } else {
-    //   setBtnDisabled(false);
-    // }
   };
   return (
     <Card>
       <form action="">
         <h2>Leave a review to rate your experience:</h2>
+        <RatingSelector select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
             onChange={handleContentChange}
